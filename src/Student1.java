45d39a4fd8064fa6b4cc40207992b01fab65a5a8
kkkssb <becky42083@gmail.com>
2023-12-21 22:08:16 +0900

    Update Student1.java
    단어고침

--- src/Student1.java
+++ src/Student1.java
@@ -1,8 +1,7 @@
 public class Student1 {
     private int mom=10000;
     private int dangn=20000;
-
-    public int sent(){
+    public int send(){
         int give=this.mom+this.dangn;
 
         return give;
