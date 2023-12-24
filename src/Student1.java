7df998ffb24f235bfba60a3bbe38ef8ded3687aa
kkkssb <becky42083@gmail.com>
2023-12-24 16:47:31 +0900

    Update Student1.java
    231224 1차 피드백후 수정값

--- src/Student1.java
+++ src/Student1.java
@@ -1,9 +1,19 @@
 public class Student1 {
-    private int mom=10000;
-    private int dangn=20000;
-    public int send(){
-        int give=this.mom+this.dangn;
+    private int mom;
+    private int dangn;
+    private int wallet;
+    private int mine;
 
-        return give;
+    public int recieveMoney() {
+        this.mom = 10000;
+        this.dangn = 20000;
+        this.wallet = this.mom + this.dangn;
+        return wallet;
+    }
+
+    public int sendMoney() {
+
+        int send = this.wallet - this.mine;
+        return send;
     }
 }
